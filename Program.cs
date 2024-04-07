950861682534adfb6f4287af24b1fb5753c330c7
Thayane Santos <tatasantoscorreia9@gmail.com>
2024-04-07 18:57:52 -0300

    tarefa concluida

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿﻿Console.WriteLine(" \n");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("SOLETRANDO \n");
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("Digite uma palavra: ");
+string palavra = Console.ReadLine()!;
+var caracteres = palavra.ToCharArray();
+string palavraSoletrada = String.Join('-', caracteres).ToUpper();
+
+Console.WriteLine($"\nSoletrando \"{palavra.ToUpper()}\": {palavraSoletrada} \n");
